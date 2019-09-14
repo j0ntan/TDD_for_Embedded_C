5250508ae789d9f17e18ae8607371ae05e1845b7
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-09-14 12:02:06 -0400

    Add runtime error & a stub

---
+++ mocks/RuntimeErrorStub.h
@@ -0,0 +1,8 @@
+#ifndef D_RuntimeErrorStub_H
+#define D_RuntimeErrorStub_H
+
+void RuntimeErrorStub_Reset(void);
+const char * RuntimeErrorStub_GetLastError(void);
+int RuntimeErrorStub_GetLastParameter(void);
+
+#endif /* D_RuntimeErrorStub_H */
