5374fbf7f189a3019870faaffadbdca468b20b87
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-09-14 18:46:57 -0400

    Refactor/extract set & clear functions

--- src/LedDriver/LedDriver.c
+++ src/LedDriver/LedDriver.c
@@ -3,12 +3,19 @@
 
 enum {ALL_LEDS_ON = ~0, ALL_LEDS_OFF = ~ALL_LEDS_ON};
 
+enum {FIRST_LED = 1, LAST_LED = 16};
+
 static uint16_t * ledsAddress;
 static uint16_t ledsImage;
 
 static void updateHardware(void)
 {
     *ledsAddress = ledsImage;
+}
+
+static int IsLedOutOfBounds(int ledNumber)
+{
+    return (ledNumber < FIRST_LED) || (ledNumber > LAST_LED);
 }
 
 void LedDriver_Create(uint16_t * address)
@@ -23,24 +30,34 @@ static uint16_t convertLedNumberToBit(int ledNumber)
     return 1 << (ledNumber - 1);
 }
 
+static void setLedImageBit(int ledNumber)
+{
+    ledsImage |= convertLedNumberToBit(ledNumber);
+}
+
 void LedDriver_TurnOn(int ledNumber)
 {
-    if (ledNumber <= 0 || ledNumber > 16)
+    if (IsLedOutOfBounds(ledNumber))
     {
         RUNTIME_ERROR("LED Driver: out-of-bounds LED", 0);
         return;
     }
 
-    ledsImage |= convertLedNumberToBit(ledNumber);
+    setLedImageBit(ledNumber);
     updateHardware();
+}
+
+static void clearLedImageBit(int ledNumber)
+{
+    ledsImage &= ~convertLedNumberToBit(ledNumber);
 }
 
 void LedDriver_TurnOff(int ledNumber)
 {
-    if (ledNumber <= 0 || ledNumber > 16)
+    if (IsLedOutOfBounds(ledNumber))
         return;
 
-    ledsImage &= ~(convertLedNumberToBit(ledNumber));
+    clearLedImageBit(ledNumber);
     updateHardware();
 }
 
