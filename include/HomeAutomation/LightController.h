74c1ade128d77e2e1dd2db13f436812e63aaf1fb
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-12-10 13:30:27 -0500

    Add LightController interface

---
+++ include/HomeAutomation/LightController.h
@@ -0,0 +1,10 @@
+#ifndef LightController_H
+#define LightController_H
+
+void LightController_Create(void);
+void LightController_Destroy(void);
+
+void LightController_On(int id);
+void LightController_Off(int id);
+
+#endif /* LightController_H */
