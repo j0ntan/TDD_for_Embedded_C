619aca1dbc85ec0230c68a14cc2959d6446bfac3
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-12-13 18:14:22 -0500

    Define days for LightScheduler

---
+++ include/HomeAutomation/LightScheduler.h
@@ -0,0 +1,9 @@
+#ifndef LightScheduler_H
+#define LightScheduler_H
+
+enum Day {
+    NONE = -1, EVERYDAY = 10, WEEKDAY, WEEKEND,
+    SUNDAY = 1, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+};
+
+#endif /* LightScheduler_H */
