beb90f187eb5d16f196b856b2a7ae2c89e320e27
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-12-20 03:11:31 -0500

    Add LightScheduler initialization interface

--- include/HomeAutomation/LightScheduler.h
+++ include/HomeAutomation/LightScheduler.h
@@ -6,4 +6,11 @@ enum Day {
     SUNDAY = 1, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
 };
 
+typedef enum Day Day;
+
+void LightScheduler_Create(void);
+void LightScheduler_Destroy(void);
+void LightScheduler_ScheduleTurnOn(int id, Day day, int minuteOfDay);
+void LightScheduler_Wakeup(void);
+
 #endif /* LightScheduler_H */
