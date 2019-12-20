63cdc347935b0e8845e79599c0b75eaabf0013dd
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-12-20 17:52:09 -0500

    Refactor light scheduler wakeup

--- src/HomeAutomation/LightScheduler.c
+++ src/HomeAutomation/LightScheduler.c
@@ -41,19 +41,29 @@ void LightScheduler_ScheduleTurnOff(int id, Day day, int minuteOfDay)
     scheduleEvent(id, day, minuteOfDay, TURN_OFF);
 }
 
+static void operateLight(ScheduledLightEvent * lightEvent)
+{
+    if (lightEvent->event == TURN_ON)
+        LightController_On(lightEvent->id);
+    else if (lightEvent->event == TURN_OFF)
+        LightController_Off(lightEvent->id);
+}
+
+static void processEventDueNow(Time * time, ScheduledLightEvent * lightEvent)
+{
+    if (lightEvent->id == UNUSED)
+        return;
+
+    if (lightEvent->minuteOfDay != time->minuteOfDay)
+        return;
+
+    operateLight(lightEvent);
+}
+
 void LightScheduler_Wakeup(void)
 {
     Time time;
     TimeService_GetTime(&time);
 
-    if (scheduledEvent.id == UNUSED)
-        return;
-
-    if (time.minuteOfDay != scheduledEvent.minuteOfDay)
-        return;
-
-    if (scheduledEvent.event == TURN_ON)
-        LightController_On(scheduledEvent.id);
-    else if (scheduledEvent.event == TURN_OFF)
-        LightController_Off(scheduledEvent.id);
+    processEventDueNow(&time, &scheduledEvent);
 }
